8ef9217197a3321729198dcc3160625ba03c54f3
ritesh2687 <riteshprajapati2687@gamil.com>
2026-04-25 10:16:30 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directories for the project
+mkdir -p src
+mkdir -p research
+
+# creating files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch README.md
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and file structure created successfully!"
